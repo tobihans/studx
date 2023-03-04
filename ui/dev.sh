7b2700d724623b3e9c8328e67845669153071f9a
Hans Tognon <learningway403@gmail.com>
2023-03-04 21:03:40 +0100

    wip: refactorin gto take dev env config out of each project

--- ui/dev.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-tmux new -d -s studx-dev \; split-window -v
-tmux send-keys -t studx-dev.0 "$(tr '\n' ' ' < .env) caddy run" ENTER
-tmux send-keys -t studx-dev.1 "$(tr '\n' ' ' < .env) npx vite --host" ENTER
-
-tmux a -t studx-dev
