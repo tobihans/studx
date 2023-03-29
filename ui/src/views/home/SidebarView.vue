01aafe502c252f76bda11cc26108b4c06af220f0
Hans Tognon <learningway403@gmail.com>
2023-03-29 21:49:38 +0100

    fix(ui): mobile sidebar org logo

--- ui/src/views/home/SidebarView.vue
+++ ui/src/views/home/SidebarView.vue
@@ -13,6 +13,7 @@ import PhGear from "~icons/ph/gear";
 import PhCalendar from "~icons/ph/calendar";
 import PhCircleWavyQuestion from "~icons/ph/circle-wavy-question";
 import PhPhoneCall from "~icons/ph/phone-call";
+import UserPlaceholderURL from "@/assets/img/user.svg";
 
 defineEmits(["start-tour"]);
 
@@ -235,7 +236,7 @@ onMounted(() => {
               >
                 <img
                   class="object-cover w-8 h-8 rounded"
-                  src="https://images.unsplash.com/photo-1502378735452-bc7d86632805?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=aa3a807e1bbdfd4364d1f449eaa96d82"
+                  :src="org?.org?.picture || OrganizationPlaceholderURL"
                   alt=""
                   aria-hidden="true"
                 />
@@ -266,7 +267,7 @@ onMounted(() => {
                     >
                       <img
                         class="object-cover w-4 h-4 rounded mr-3"
-                        src="https://images.unsplash.com/photo-1502378735452-bc7d86632805?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=aa3a807e1bbdfd4364d1f449eaa96d82"
+                        :src="organization.picture || OrganizationPlaceholderURL"
                         alt=""
                         aria-hidden="true"
                       />
