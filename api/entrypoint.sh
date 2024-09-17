4c62be728d3fe1ca5485dcefc00d3c7dc0956b9e
Hans B. K. Tognon <hans.tognon@pitchcardinal.com>
2024-09-17 09:57:00 +0100

    feat: run api & celery together for now

--- api/entrypoint.sh
+++ api/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 python manage.py collectstatic --no-input
+# FIXME: starting celery in background here, since it doesn't have that much task for now.
+celery -A core worker > /var/celery.log 2>&1 &
 gunicorn core.wsgi:application --bind 0.0.0.0:"${PORT:-8000}"
